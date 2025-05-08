65eef5a8a250e592c82bf2e9841e398e7dc203da
Rafał Bastek <rafun2014@gmail.com>
2025-05-08 18:05:09 +0200

    Fixing Code & Add help main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def is_palindrome(text):
-    reverse_order = ""
-    for letter in text:
-        reverse_order += letter
+    """This code checks if the string is a
+    palindrome!
+    It takes the argument and asigns the value to a new
+    variable called 'reverse_order' by iterating the whole string backwards
+    and checking if value is same as the original string!"""
+    reverse_order = text[::-1]
+    print(reverse_order)
     if reverse_order == text:
         return True
     else: 
         return False
+    
 
-print(is_palindrome("But"))
+print(is_palindrome("kajak"))
